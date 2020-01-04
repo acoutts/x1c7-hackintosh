32db1c7835a35a099fcc51969c297bdfbbc6d81d
Andrew Coutts <andrewjohncoutts@gmail.com>
2020-01-03 20:48:37 -0500

    Updates to OC

--- sync-efi.sh
+++ sync-efi.sh
@@ -1,2 +1,7 @@
-rm -rf /Volumes/EFI/EFI/*
-cp -r ./EFI /Volumes/EFI/
+# Get latest build for OC
+cp -f ../OpenCorePkg/Binaries/DEBUG/OpenCore.efi ./EFI/OC/OpenCore.efi
+cp -f ../OpenCorePkg/Binaries/DEBUG/BOOTx64.efi ./EFI/BOOT/BOOTx64.efi
+
+# Sync files to EFI partition
+rm -rf /Volumes/OC/EFI/*
+cp -r ./EFI /Volumes/OC/
