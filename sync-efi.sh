9540fcb74f4409ee64a82c5ec0eb6df67d30c66c
Andrew Coutts <andrewjohncoutts@gmail.com>
2020-01-03 23:13:31 -0500

    Logging issue

--- sync-efi.sh
+++ sync-efi.sh
@@ -1,6 +1,6 @@
 # Get latest build for OC
-cp -f ../OpenCorePkg/Binaries/DEBUG/OpenCore.efi ./EFI/OC/OpenCore.efi
-cp -f ../OpenCorePkg/Binaries/DEBUG/BOOTx64.efi ./EFI/BOOT/BOOTx64.efi
+#cp -f ../OpenCorePkg/Binaries/DEBUG/OpenCore.efi ./EFI/OC/OpenCore.efi
+#cp -f ../OpenCorePkg/Binaries/DEBUG/BOOTx64.efi ./EFI/BOOT/BOOTx64.efi
 
 # Sync files to EFI partition
 rm -rf /Volumes/OC/EFI/*
